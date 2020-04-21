8867bf4e48c8497d792fc2f6fc378d52556f8220
Michał Budzyński <michal@virtualdesign.pl>
2020-04-21 02:44:31 +0200

    hh:mm:ss

--- src/ui/Timer.ts
+++ src/ui/Timer.ts
@@ -1,9 +1,14 @@
 import {html} from "./html.js";
 
 export function Timer(time: number) {
+    const hours = Math.floor(time / 60 / 60);
+    const minutes = Math.floor(time / 60) - hours * 60;
+    const seconds = time % 60;
     return html`
         <span class="timer">
-            ${time}
+            ${hours.toString().padStart(2, "0")}:${minutes
+                .toString()
+                .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}
         </span>
     `;
 }

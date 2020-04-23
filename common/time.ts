8791f0766da07d35cc693ae09792971a143cae7e
Staś Małolepszy <stas@piesku.com>
2020-04-23 12:25:00 +0200

    Remove unused time formatting functions

--- common/time.ts
+++ common/time.ts
@@ -1,21 +1,3 @@
-export function seconds_to_time(Seconds: number) {
-    let hours = Math.floor(Seconds / 60 / 60);
-    let minutes = Math.floor(Seconds / 60) - hours * 60;
-    let seconds = Math.floor(Seconds % 60);
-    return `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-}
-
-export function seconds_to_short_time(Seconds: number) {
-    let hours = Math.floor(Seconds / 60 / 60);
-    let minutes = Math.floor(Seconds / 60) - hours * 60;
-    let seconds = Math.floor(Seconds % 60);
-    return `${hours > 0 ? hours.toString().padStart(2, "0") + ":" : ""}${
-        minutes > 0 ? minutes.toString().padStart(2, "0") + ":" : ""
-    }${seconds.toString().padStart(2, "0")}`;
-}
-
 const MINUTE = 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;

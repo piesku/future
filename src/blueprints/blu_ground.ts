cdf4e7b086757a4231d6767af7a5c69880e1ef74
Michał Budzyński <michal@virtualdesign.pl>
2020-04-24 02:29:00 +0200

    move camera on level transition

--- src/blueprints/blu_ground.ts
+++ src/blueprints/blu_ground.ts
@@ -1,24 +1,27 @@
-import {float} from "../../common/random.js";
+import {Vec4} from "../../common/math.js";
 import {render_diffuse} from "../components/com_render_diffuse.js";
 import {Blueprint} from "../core.js";
 import {Game} from "../game.js";
 
-export function blueprint_ground(game: Game, width: number, height: number) {
+export function blueprint_ground(game: Game, size: number, color: () => Vec4) {
     let Children: Blueprint[] = [];
 
-    for (let x = -width / 2; x < width / 2; x++) {
-        for (let y = -height / 2; y < height / 2; y++) {
-            Children.push({
-                Translation: [x, 0, y],
-                Using: [
-                    render_diffuse(game.MaterialDiffuseGouraud, game.MeshCube, [
-                        1,
-                        Math.min(1, 0.8 + float() * 0.3),
-                        0.1 + float() * 0.4,
-                        1,
-                    ]),
-                ],
-            });
+    for (let x = -size / 2; x < size / 2; x++) {
+        for (let y = -size / 2; y < size / 2; y++) {
+            Children.push(
+                {
+                    Translation: [x, 0, y],
+                    Using: [render_diffuse(game.MaterialDiffuseGouraud, game.MeshCube, color())],
+                },
+                {
+                    Translation: [x, -(y + size / 2), size / 2 - 1],
+                    Using: [render_diffuse(game.MaterialDiffuseGouraud, game.MeshCube, color())],
+                },
+                {
+                    Translation: [size / 2 - 1, -(y + size / 2), x],
+                    Using: [render_diffuse(game.MaterialDiffuseGouraud, game.MeshCube, color())],
+                }
+            );
         }
     }
     return <Blueprint>{

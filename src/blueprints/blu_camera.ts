0d56f2440bbab735ad8d29f0defd63850bdb3960
Michał Budzyński <michal@virtualdesign.pl>
2020-04-24 02:29:00 +0200

    move camera on level transition

--- src/blueprints/blu_camera.ts
+++ src/blueprints/blu_camera.ts
@@ -1,9 +1,16 @@
+import {Vec3} from "../../common/math.js";
 import {camera} from "../components/com_camera.js";
+import {time_control} from "../components/com_time_control.js";
 import {Blueprint} from "../core.js";
 import {Game} from "../game.js";
 
-export function blueprint_camera(game: Game) {
+export let levels_space = 8;
+
+export function blueprint_camera(game: Game, position: Vec3, transition_time: number) {
+    let end_position = position.slice() as Vec3;
+    end_position[1] += levels_space;
     return <Blueprint>{
+        Using: [time_control(transition_time - 100, transition_time, position, end_position)],
         Children: [
             {
                 Rotation: [-0.28, 0.364, 0.116, 0.88],

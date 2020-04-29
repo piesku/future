c5348bd3a1e53a9676c7c79cc65cb6aa46232a26
Staś Małolepszy <stas@piesku.com>
2020-04-29 16:35:23 +0200

    Use init_generators() in StartNewGame, too

--- src/actions.ts
+++ src/actions.ts
@@ -93,43 +93,11 @@ export function dispatch(game: Game, action: Action, payload: unknown) {
                 game.FirstRun = true;
                 game.HasWon = false;
                 game.EraCurrent = 0;
-
                 game.Rewinding = false;
                 game.TpsCurrent = 0;
                 game.TimeEarned = 0;
                 game.TimeEarnedOffline = 0;
-                game.Generators = [
-                    {
-                        id: 0,
-                        count: 1,
-                        unlocked: true,
-                    },
-                    {
-                        id: 1,
-                        count: 0,
-                        unlocked: true,
-                    },
-                    {
-                        id: 2,
-                        count: 0,
-                        unlocked: false,
-                    },
-                    {
-                        id: 3,
-                        count: 0,
-                        unlocked: false,
-                    },
-                    {
-                        id: 4,
-                        count: 0,
-                        unlocked: false,
-                    },
-                    {
-                        id: 5,
-                        count: 0,
-                        unlocked: false,
-                    },
-                ];
+                game.Generators = init_generators();
 
                 game_save(game);
                 scene_stage(game);

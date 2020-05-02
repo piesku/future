356611b10e672de84935fc1ea6ff2418e8b41e6f
Staś Małolepszy <stas@piesku.com>
2020-05-02 15:09:55 +0200

    Enable VAO extension

--- src/components/com_render_textured.ts
+++ src/components/com_render_textured.ts
@@ -1,5 +1,5 @@
 import {Material, Mesh} from "../../common/material.js";
-import {GL_CW} from "../../common/webgl.js";
+import {GL_ARRAY_BUFFER, GL_CW, GL_ELEMENT_ARRAY_BUFFER, GL_FLOAT} from "../../common/webgl.js";
 import {TexturedLayout} from "../../materials/layout_textured.js";
 import {Entity, Game} from "../game.js";
 import {Has} from "./com_index.js";
@@ -22,6 +22,39 @@ export function render_textured(
     texture: WebGLTexture
 ) {
     return (game: Game, entity: Entity) => {
+        if (!vaos.has(mesh)) {
+            // We only need to create the VAO once.
+            let vao = game.ExtVao.createVertexArrayOES()!;
+            game.ExtVao.bindVertexArrayOES(vao);
+
+            game.GL.bindBuffer(GL_ARRAY_BUFFER, mesh.VertexBuffer);
+            game.GL.enableVertexAttribArray(material.Locations.VertexPosition);
+            game.GL.vertexAttribPointer(
+                material.Locations.VertexPosition,
+                3,
+                GL_FLOAT,
+                false,
+                0,
+                0
+            );
+
+            game.GL.bindBuffer(GL_ARRAY_BUFFER, mesh.TexCoordBuffer);
+            game.GL.enableVertexAttribArray(material.Locations.VertexTexCoord);
+            game.GL.vertexAttribPointer(
+                material.Locations.VertexTexCoord,
+                2,
+                GL_FLOAT,
+                false,
+                0,
+                0
+            );
+
+            game.GL.bindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.IndexBuffer);
+
+            game.ExtVao.bindVertexArrayOES(null);
+            vaos.set(mesh, vao);
+        }
+
         game.World.Mask[entity] |= Has.Render;
         game.World.Render[entity] = {
             Kind: RenderKind.Textured,

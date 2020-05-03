df0bdda6ddfa83d0f47e966650be36f3b9d81543
Staś Małolepszy <stas@piesku.com>
2020-05-03 10:25:52 +0200

    Window

--- src/ui/Clock.ts
+++ src/ui/Clock.ts
@@ -2,75 +2,54 @@ import {html} from "../../common/html.js";
 import {MAX_SECONDS} from "../../common/time.js";
 import {Action} from "../actions.js";
 import {Game} from "../game.js";
+import {Window} from "./Window.js";
 
 export function Clock(game: Game) {
-    return html`
-        <div
-            class="window"
-            data-name="clock"
-            style="
-                width: 300px;
-                margin: 16px;
-                ${game.WindowPositions["clock"] &&
-            `
-                    position: relative;
-                    top: ${game.WindowPositions["clock"][0]}px;
-                    left: ${game.WindowPositions["clock"][1]}px;
-                    z-index: ${game.WindowPositions["clock"][2]};
-                `}
-            "
-        >
-            <div
-                class="title-bar"
-                onmousedown="event.stopPropagation(); $(${Action.DraggingStart}, 'clock');"
-                onmouseup="event.stopPropagation(); $(${Action.DraggingStop});"
-            >
-                <div class="title-bar-text">
-                    Clock
+    return Window(
+        game,
+        "Clock",
+        html`
+            ${game.DateCurrent > game.DateGoal
+                ? `<p>Congratulations! You have reached the future.</p>`
+                : `<p>To win the game, reach the future!</p>`}
+            <fieldset class="field-row">
+                ${game.DateCurrent <= MAX_SECONDS
+                    ? `
+                        <legend>
+                            Current Date
+                        </legend>
+                        <div class="field-row" style="justify-content: space-evenly;">
+                            <div id="clock-current-year" style="width: 60px; text-align: center;"></div>
+                            <div id="clock-current-day" style="width: 140px; text-align: center;"></div>
+                            <div id="clock-current-time" style="width: 50px; text-align: center;"></div>
+                        </div>
+                    `
+                    : `
+                        <legend>
+                            Years into the Far Future
+                        </legend>
+                        <div class="field-row" id="clock-current-year" style="overflow-wrap: anywhere;"></div>
+                    `}
+            </fieldset>
+            <fieldset class="field-row">
+                <legend>
+                    Goal Date
+                </legend>
+                <div class="field-row" style="justify-content: space-evenly;">
+                    <div id="clock-future-year" style="width: 60px; text-align: center;"></div>
+                    <div id="clock-future-day" style="width: 140px; text-align: center;"></div>
+                    <div id="clock-future-time" style="width: 50px; text-align: center;"></div>
                 </div>
-            </div>
-            <div class="window-body">
-                ${game.DateCurrent > game.DateGoal
-                    ? `<p>Congratulations! You have reached the future.</p>`
-                    : `<p>To win the game, reach the future!</p>`}
-                <fieldset class="field-row">
-                    ${game.DateCurrent <= MAX_SECONDS
-                        ? `
-                            <legend>
-                                Current Date
-                            </legend>
-                            <div class="field-row" style="justify-content: space-evenly;">
-                                <div id="clock-current-year" style="width: 60px; text-align: center;"></div>
-                                <div id="clock-current-day" style="width: 140px; text-align: center;"></div>
-                                <div id="clock-current-time" style="width: 50px; text-align: center;"></div>
-                            </div>
-                        `
-                        : `
-                            <legend>
-                                Years into the Far Future
-                            </legend>
-                            <div class="field-row" id="clock-current-year" style="overflow-wrap: anywhere;"></div>
-                        `}
-                </fieldset>
-                <fieldset class="field-row">
-                    <legend>
-                        Goal Date
-                    </legend>
-                    <div class="field-row" style="justify-content: space-evenly;">
-                        <div id="clock-future-year" style="width: 60px; text-align: center;"></div>
-                        <div id="clock-future-day" style="width: 140px; text-align: center;"></div>
-                        <div id="clock-future-time" style="width: 50px; text-align: center;"></div>
-                    </div>
-                </fieldset>
-                ${game.DateCurrent > game.DateGoal &&
-                `
-                    <div class="field-row" style="justify-content: center;">
-                        <button onmouseup="event.stopPropagation(); $(${Action.StartNewGame});">
-                            Start a New Game
-                        </button>
-                    </div>
-                `}
-            </div>
-        </div>
-    `;
+            </fieldset>
+            ${game.DateCurrent > game.DateGoal &&
+            `
+                <div class="field-row" style="justify-content: center;">
+                    <button onmouseup="event.stopPropagation(); $(${Action.StartNewGame});">
+                        Start a New Game
+                    </button>
+                </div>
+            `}
+        `,
+        300
+    );
 }
---
+++ src/ui/Window.ts
@@ -0,0 +1,35 @@
+import {html} from "../../common/html.js";
+import {Action} from "../actions.js";
+import {Game} from "../game.js";
+
+export function Window(game: Game, title: string, content: string, width = 250) {
+    return html`
+        <div
+            class="window"
+            style="
+                width: ${width}px;
+                margin: 16px;
+                ${game.WindowPositions[title] &&
+            `
+                    position: relative;
+                    top: ${game.WindowPositions[title][0]}px;
+                    left: ${game.WindowPositions[title][1]}px;
+                    z-index: ${game.WindowPositions[title][2]};
+                `}
+            "
+        >
+            <div
+                class="title-bar"
+                onmousedown="event.stopPropagation(); $(${Action.DraggingStart}, '${title}');"
+                onmouseup="event.stopPropagation(); $(${Action.DraggingStop});"
+            >
+                <div class="title-bar-text">
+                    ${title}
+                </div>
+            </div>
+            <div class="window-body">
+                ${content}
+            </div>
+        </div>
+    `;
+}
